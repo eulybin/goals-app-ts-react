80073746a157225340a10d6c089158e7ef5d8bb5
Egor Ulybin <egorulybin@gmail.com>
2025-10-16 17:50:50 +0200

    working with conditional rendering and union types for props

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from './CourseGoal';
+import InfoBox from './InfoBox';
 import { type CourseGoal as CourseGoalType } from '../types';
+import type { ReactNode } from 'react';
 
 type CourseGoalListProps = {
     goals: CourseGoalType[];
@@ -7,15 +9,30 @@ type CourseGoalListProps = {
 };
 
 export default function CourseGoalList({ goals, onDelete }: CourseGoalListProps) {
+    if (goals.length === 0) {
+        return <InfoBox mode='hint'>No course goals yet. Start adding some!</InfoBox>;
+    }
+
+    let warningBox: ReactNode;
+    if (goals.length >= 4) {
+        warningBox = (
+            <InfoBox mode='warning' severity='medium'>
+                You already have a few goals. Don't over do it.
+            </InfoBox>
+        );
+    }
     return (
-        <ul>
-            {goals.map((goal) => (
-                <li key={goal.id}>
-                    <CourseGoal id={goal.id} onDelete={onDelete} title={goal.title}>
-                        <p>{goal.description}</p>
-                    </CourseGoal>
-                </li>
-            ))}
-        </ul>
+        <>
+            {warningBox}
+            <ul>
+                {goals.map((goal) => (
+                    <li key={goal.id}>
+                        <CourseGoal id={goal.id} onDelete={onDelete} title={goal.title}>
+                            <p>{goal.description}</p>
+                        </CourseGoal>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,34 @@
+import type { ReactNode } from 'react';
+
+type HintInfoBoxProps = {
+    mode: 'hint';
+    children: ReactNode;
+};
+
+type WarningInfoBoxProps = {
+    mode: 'warning';
+    severity: 'low' | 'medium' | 'high';
+    children: ReactNode;
+};
+
+type InfoBoxProps = HintInfoBoxProps | WarningInfoBoxProps;
+
+export default function InfoBox(props: InfoBoxProps) {
+    const { mode, children } = props;
+    if (mode === 'hint') {
+        return (
+            <aside className='infobox infobox-hint'>
+                <p>{children}</p>
+            </aside>
+        );
+    }
+
+    const { severity } = props;
+
+    return (
+        <aside className={`infobox infobox-warning warning--${severity}`}>
+            <h2>Warning</h2>
+            <p>{children}</p>
+        </aside>
+    );
+}
